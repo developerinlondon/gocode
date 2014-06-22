f86f39ab60c19595f75effcaf31ee57c792af3d1
Nayeem Syed <developerinlondon@gmail.com>
2014-06-22 23:51:08 +0100

    fixed typo

--- src/github.com/nf/hello/hello.go
+++ src/github.com/nf/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import {
+  "fmt"
+  "github.com/nf/string"
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Println(string.Reverse("hello, world\n"))
 }
--- src/github.com/nf/string/string.go
+++ src/github.com/nf/string/string.go
@@ -1,4 +1,4 @@
-package String
+package string
 
 func Reverse( s string) string {
   b := []byte(s)
